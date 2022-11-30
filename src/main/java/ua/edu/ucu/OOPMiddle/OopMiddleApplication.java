cfa4d03b5a07d42519cb896436adb143637c271f
Shchur Oleksandr <Olexander.Shchur@gmail.com>
2022-11-30 17:06:46 +0200

    Initial commit

---
+++ src/main/java/ua/edu/ucu/OOPMiddle/OopMiddleApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ucu.OOPMiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopMiddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopMiddleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/ucu/OOPMiddle/OopMiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ucu.OOPMiddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopMiddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
